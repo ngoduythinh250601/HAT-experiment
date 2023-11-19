e216e706a5edb7060c4efffad1c08a7e09e47935
ngoduythinh250601 <thinhitk15nbk@gmail.com>
2023-11-19 12:23:33 +0700

    update additional models, options/test YAML file, fix app.py

--- deploy/app.py
+++ deploy/app.py
@@ -13,7 +13,8 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 class Info:
-    def __init__(self, models_dir, default_model_name):
+    def __init__(self, cwd, models_dir, default_model_name):
+        self.cwd = cwd
         self.models_dir = models_dir
         self.default_model_name = default_model_name
     
@@ -23,81 +24,97 @@ class Info:
     def set_Models_dir(self, models_dir):
         self.models_dir = models_dir
 
+class ModelPlaceholder:
+    def __init__(self, info : Info):
+        self.info = info
+        self.model = Model(info.cwd, os.path.join(info.models_dir, info.default_model_name + '.yml'))
+        
+    def set_Info(self, info : Info):
+        self.info = info
+
+    def change_model(self, model_name):
+        ans = dict()
+        if model_name.startswith("HAT-S"):
+            try:
+                for file_name in os.listdir(info.models_dir):
+                    if model_name in file_name:
+                        self.model = Model(info.cwd, os.path.join(self.info.models_dir, model_name + '.yml'))
+                        ans["status"] = "success"
+                        ans["msg"] = "Model has changed: " + info.default_model_name + ".yml"
+                        return ans
+                ans["status"] = "failed"
+                ans["msg"] = "Can't find model!"
+            except:
+                ans["status"] = "failed"
+                ans["msg"] = "Cannot change model!"
+        else:
+            ans["status"] = "failed"
+            ans["msg"] = "Invalid data!"
+        return ans
+ 
 
 @app.route('/')
 def hello():
    return 'All is well if you see this line!'
 
 
-@app.route("/change_model", methods=["POST"])
-def change_model():
-    ans = dict()
-    try:
-        data = request.get_json()
-        print("request from user: ", data)
-        if str(data['modelName']).startswith("HAT-S"):
-            model_name = str(data['modelName'])
-            if info.default_model_name == model_name:
-                ans["status"] = "success"
-                ans["msg"] = "model had changed: " + model_name
-                return jsonify(ans)
-            try:
-                for file_name in os.listdir(info.models_dir):
-                    if model_name in file_name:
-                        model = Model(cwd, os.path.join(info.models_dir, file_name))
-                        print("Model has been changed:", model.model.opt['name'])
-                        ans["status"] = "success"
-                        ans["msg"] = "model had changed: " + model_name + ".yml"
-                        info.default_model_name = model_name
-                        return jsonify(ans)
-                ans["status"] = "failed"
-                ans["msg"] = "can't find model"
-            except:
-                ans["status"] = "failed"
-                ans["msg"] = "can't change model"
-        else:
-            ans["status"] = "failed"
-            ans["msg"] = "invalid data"
-    except:
-        ans["status"] = "failed"
-        ans["msg"] = "fetch issue"
-    return jsonify(ans)
-
 @app.route("/upload", methods=["POST"])
 def upload():
     result = dict()
     try:
-        data = request.files['image']
-        pil_image = Image.open(data)
-        numpy_array = np.asarray(pil_image)
-        cv2_image = cv2.cvtColor(numpy_array, cv2.COLOR_RGB2BGR).astype(float) / 255
-        
+        image_data = request.files['image']
+        model_name = request.form.get('model_name', '')
+        print(request.files)
+        print(f"model_name = {model_name}")
         try:
-            sr_cv2_img = model.process(cv2_image)
-            np_array = cv2.cvtColor(sr_cv2_img, cv2.COLOR_BGR2RGB)
-            sr_pil_image = Image.fromarray(np_array)
-            
-            image_bytes = BytesIO()
-            sr_pil_image.save(image_bytes, format='png')
-            image_bytes.seek(0)
-            
-            result["status"] = "success"
-            result["msg"] = "success"
-            return send_file(image_bytes, mimetype='image/png')
-            # result["img"] = sr_pil_image
+            try:
+                if (model_name != info.default_model_name):
+                    status = model_placeholder.change_model(model_name)
+                    if (status["status"] == "success"):
+                        print("Model has changed:", model_placeholder.model.opt['name'])
+                        info.set_Default_model_name(model_name)
+                        model_placeholder.set_Info(info)
+                    else:
+                        print(status)
+                        return jsonify(status)
+            except:
+                result["status"] = "failed"   
+                result["msg"] = "Cannot change model!"
+                print(result)
+                return jsonify(result)
+
+            pil_image = Image.open(image_data)
+            numpy_array = np.asarray(pil_image)
+            cv2_image = cv2.cvtColor(numpy_array, cv2.COLOR_RGB2BGR).astype(float) / 255
+            try:
+                sr_cv2_img = model_placeholder.model.process(cv2_image)
+                np_array = cv2.cvtColor(sr_cv2_img, cv2.COLOR_BGR2RGB)
+                sr_pil_image = Image.fromarray(np_array)
+                
+                image_bytes = BytesIO()
+                sr_pil_image.save(image_bytes, format='png')
+                image_bytes.seek(0)
+                
+                result["status"] = "success"
+                result["msg"] = "success"
+                print("Successfully increase photo quality! Returning...")
+                return send_file(image_bytes, mimetype='image/png')
+            except:
+                result["status"] = "failed"   
+                result["msg"] = "Insufficient GPU memory!"
         except:
             result["status"] = "failed"   
-            result["msg"] = "model process encountered a problem"
+            result["msg"] = "Image data cannot be read!"
     except:
         result["status"] = "failed"   
-        result["msg"] = "image loading problem"
+        result["msg"] = "The request is not in the correct format!"
     print(result)
     return jsonify(result)
 
 
 if __name__ == "__main__":
     cwd = os.getcwd()
-    info = Info(models_dir=os.path.join(cwd,"options"), default_model_name="HAT-S_SRx4_from_scratch")
-    model = Model(cwd, os.path.join(info.models_dir, info.default_model_name + '.yml'))
-    print("Default model name:", model.model.opt['name'])
+    info = Info(cwd=cwd, models_dir=os.path.join(cwd,"options"), default_model_name="HAT-S_SRx4_from_scratch")
+    model_placeholder = ModelPlaceholder(info)
+    print("Default model name:", model_placeholder.model.opt['name'])
     app.run(port=5000)
